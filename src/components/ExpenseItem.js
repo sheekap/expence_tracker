b7cb2fe20a1ff032f1e8c6e31381cd6d94367b34
Sheeka Patak <sheeka.patak@gmail.com>
2022-06-01 19:20:44 +0100

    🛠💅  💅🏾 first attempt at an expense item and some styling

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import "./ExpenseItem.css"
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+      </div>
+      <div className="expense-item__price">$249.67</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
