6a64eeaf4c5e7672b542a595542a243493f6bb7b
Sheeka Patak <sheeka.patak@gmail.com>
2022-06-01 19:39:54 +0100

    🔧 use JS to make ExpenseItem values a little more dynamic, introduce placeholders

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import "./ExpenseItem.css"
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>March 28 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
+        <h2>{expenseTitle}</h2>
       </div>
-      <div className="expense-item__price">$249.67</div>
+      <div className="expense-item__price">${expenseAmount}</div>
     </div>
   );
 }
