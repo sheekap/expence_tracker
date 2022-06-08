b6ea2a03bb545066784412f444a74233d4a3eec3
Sheeka Patak <sheeka.patak@gmail.com>
2022-06-08 09:43:57 +0100

    add new expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from 'react';
+
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -30,6 +33,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses expense_data={expenses} />
     </div>
   );
