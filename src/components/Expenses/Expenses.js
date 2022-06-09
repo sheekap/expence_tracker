28979f1680eaea5762919bbc8c4b669df0eec37d
Sheeka Patak <sheeka.patak@gmail.com>
2022-06-09 08:54:01 +0100

    assignment - pass data from ExpensesFilter to Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,22 +1,35 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpensesFilter";
+
 import "./Expenses.css"
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2019');
   const expenses = props.expense_data;
 
+  const changeFilteredYearHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(filteredYear);
+  };
+
   return (
-    <Card className="expenses">
-      <div>
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter selected={filteredYear} onChangeSelectedYear={changeFilteredYearHandler} />
         <div>
-          {/* TODO: use a loop if I can */}
-          <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-          <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-          <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-          <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+          <div>
+            {/* TODO: use a loop if I can */}
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </div>
   );
 }
 
